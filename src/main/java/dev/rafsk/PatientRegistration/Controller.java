ac1a6e2b3a24e91c564cec79fa3241d27c24cf13
Rafael <rafaelsaibro1@gmail.com>
2026-04-28 20:11:40 -0300

    Add Controller

---
+++ src/main/java/dev/rafsk/PatientRegistration/Controller.java
@@ -0,0 +1,21 @@
+package dev.rafsk.PatientRegistration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "First msg in this route";
+    }
+
+
+
+
+
+}
