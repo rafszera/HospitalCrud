652d3bb231e02554d8eaaa747c6c096dc40d40e4
Rafael <rafaelsaibro1@gmail.com>
2026-04-28 19:28:05 -0300

    first commit

---
+++ src/main/java/dev/rafsk/PatientRegistration/PatientRegistrationApplication.java
@@ -0,0 +1,13 @@
+package dev.rafsk.PatientRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRegistrationApplication.class, args);
+	}
+
+}
