2c853d2a3f031f0bfb598d642a51ccef86cd0999
Rafael <rafaelsaibro1@gmail.com>
2026-04-29 19:47:45 -0300

    Create Patient entity based on PatientModel

---
+++ src/main/java/dev/rafsk/HospitalCRUD/PatientModel.java
@@ -0,0 +1,59 @@
+package dev.rafsk.HospitalCRUD;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_patient_registration")
+public class PatientModel {
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     Long id;
+     String name;
+     String email;
+     int age;
+
+
+
+    public PatientModel(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public PatientModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
