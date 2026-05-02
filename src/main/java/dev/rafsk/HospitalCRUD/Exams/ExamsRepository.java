11459dbcec9276c4b59c75799f4d343d6b347728
Rafael Saibro <rafaelsaibro1@gmail.com>
2026-05-02 20:14:31 -0300

    build: Connect DB H2 to aplicattion

---
+++ src/main/java/dev/rafsk/HospitalCRUD/Exams/ExamsRepository.java
@@ -0,0 +1,6 @@
+package dev.rafsk.HospitalCRUD.Exams;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamsRepository extends JpaRepository <ExamsModel, Long> {
+}
