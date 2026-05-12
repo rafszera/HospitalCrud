55d745fc9a7c4fe26c0d8ae4a0c00d20d62fac9d
Rafael Saibro <rafaelsaibro1@gmail.com>
2026-05-12 20:53:44 -0300

    fix: add Endpoints

--- src/main/java/dev/rafsk/HospitalCRUD/Patients/PatientController.java
+++ src/main/java/dev/rafsk/HospitalCRUD/Patients/PatientController.java
@@ -1,17 +1,34 @@
 package dev.rafsk.HospitalCRUD.Patients;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class PatientController {
 
-
     @GetMapping("/welcome")
-    public String welcome(){
-        return "First msg in this route";
+        public String welcome(){
+            return "First msg in this route";
+    }
+    @PostMapping("/create")
+        public String createPatient(){
+            return "Patient Created";
+    }
+    @GetMapping("/allPatients")
+        public String showAllPatient(){
+            return "Show Patients";
+    }
+    @GetMapping("/allID")
+        public String showAllPatientsID(){
+        return "Patient ID: ";
+    }
+    @PutMapping("/changebyID")
+        public String changePatientByID() {
+            return "Change Patient by ID";
+        }
+    @DeleteMapping("/deleteByID")
+        public String deleteByID() {
+            return "Delete Patient by ID";
     }
 
 
